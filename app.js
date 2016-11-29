e8d45b504f09ddb4a918d0754ee6ed2740831e55
Remil Marzan <remilonwheels@gmail.com>
2016-11-29 14:44:57 -0800

    finished question 1 and made text blue

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert('I AM ALIVE');
+'use strict';
+
+var question1 = prompt('Does Remil have any cats?').toLowerCase();
+
+if (question1 === 'yes' || question1 === 'y') {
+  alert('Sorry but you\'re wrong, Remil doesn\'t have any pets');
+} else if (question1 === 'no' || question1 === 'n') {
+  alert('You are right, Remil doesn\'t have any pets but had a family dog growing up');
+} else {
+  alert('Try to answer with a yes/y or no/n next time');
+}
