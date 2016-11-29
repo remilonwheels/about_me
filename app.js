531e6c2ecea6e5aebe17db6a2c3533cec47c7fcd
Remil Marzan <remilonwheels@gmail.com>
2016-11-29 14:55:52 -0800

    renamed variable for user response

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var question1 = prompt('Does Remil have any cats?').toLowerCase();
+var response1 = prompt('Does Remil have any cats?').toLowerCase();
 
-if (question1 === 'yes' || question1 === 'y') {
+if (response1 === 'yes' || response1 === 'y') {
   alert('Sorry but you\'re wrong, Remil doesn\'t have any pets');
-} else if (question1 === 'no' || question1 === 'n') {
+} else if (response1 === 'no' || response1 === 'n') {
   alert('You are right, Remil doesn\'t have any pets but had a family dog growing up');
 } else {
   alert('Try to answer with a yes/y or no/n next time');
